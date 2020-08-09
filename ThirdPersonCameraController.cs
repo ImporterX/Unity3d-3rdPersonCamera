6692a33a9ab32322506651fd95b45b3f9db31546
ImporterX <46138228+ImporterX@users.noreply.github.com>
2020-08-09 19:34:16 +0200

    Add files via upload

---
+++ ThirdPersonCameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+        
+
+        transform.LookAt(Target);
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            mouseX = Mathf.Clamp(mouseX, -50, 50);
+        }
+        else
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+
+    }
+}
